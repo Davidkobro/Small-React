eaf740cc139d6eacdb9056d925c49c7467838aae
David Kobrosky <david.kobrosky@vmny-david.kobrosky>
2019-05-08 17:44:21 -0400

    drag drop boiler

---
+++ dragdrop/src/AppDragDrop.js
@@ -0,0 +1,13 @@
+import React, { Component} from 'react';
+
+export default class AppDragDrop extents Component{
+
+  render() {
+    return(
+        <div className="container-drag">
+
+        </div>;
+
+    );
+  }
+}
